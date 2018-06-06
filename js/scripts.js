2251c20b49e0773ab1567947f54acb3b96cad40f
Abel Trotter and Eliran Maimon <abel@>
2018-06-06 15:29:40 -0700

    add check for numbers and for special characters

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 const VOWELS = ["a", "e", "i", "o", "u", "y"];
 
 //Business Logic
-function toPigLatin(input) {
+function getPhrase(input) {
   console.log("Input: " + input);
   var words = parsePhrase(input);
   console.log("Words: " + words);
-  parseWords(words);
+  return words;
 }
 
 function parsePhrase(userInput) {
@@ -13,37 +13,57 @@ function parsePhrase(userInput) {
   return words;
 }
 
-function parseWords(wordsArray) {
+function toPigLatin(wordsArray) {
   var finishedWords = [];
   var pieces = [];
+  wordsArray = removePunctuation(wordsArray);
   for(var i = 0; i < wordsArray.length; i++) {
+    if (!isNaN(parseInt(wordsArray[i]))) {
+      finishedWords.push(wordsArray[i]);
+      continue;
+    }
     var lettersArray = wordsArray[i].split("");
     console.log("letters array: " + lettersArray);
     for(var j = 0; j < lettersArray.length; j++) {
+      console.log(pieces);
       if (VOWELS.includes(lettersArray[0]) && lettersArray[0] !== "y") {
         var newWord = addWayToEnd(wordsArray[i]);
         console.log(newWord);
         finishedWords.push(newWord);
-        var pieces = [];
         break;
-      } else if (!VOWELS.includes(lettersArray[j]) && lettersArray[j] === "q" && lettersArray[j+1] === "u") {
+      } else if (!VOWELS.includes(lettersArray[j]) && lettersArray[j] === "q" && lettersArray[j + 1] === "u") {
         pieces.push(lettersArray[j], lettersArray[j + 1]);
         var newWord = createWord(pieces, wordsArray[i]);
         finishedWords.push(newWord);
-        var pieces = [];
         break;
-      } else if (!VOWELS.includes(lettersArray[j])) {
+      } else if (!VOWELS.includes(lettersArray[j]) || lettersArray[0] === "y") {
         pieces.push(lettersArray[j]);
+        var newWord = createWord(pieces, wordsArray[i]);
+        finishedWords.push(newWord);
+        break;
       } else if (VOWELS.includes(lettersArray[j])) {
         var newWord = createWord(pieces, wordsArray[i]);
         finishedWords.push(newWord);
-        var pieces = [];
         break;
         console.log("New word: " + newWord);
       }
     }
+    pieces = [];
   }
   console.log(finishedWords);
+}
+
+function removePunctuation(wordList) {
+  var pattern = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/;
+  var newList = wordList.map(function(word) {
+    var position = word.search(pattern);
+    if (position >= 0) {
+      symbol = word.substring(position);
+      word = word.replace(symbol, "");
+    }
+    return word;
+  });
+  return newList;
 }
 
 function addWayToEnd(word) {
@@ -76,7 +96,8 @@ $(document).ready(function() {
   $("#pig-latin").submit(function(event) {
     event.preventDefault();
     var userInput = $("#input").val();
-    var result = toPigLatin(userInput);
+    var words = getPhrase(userInput);
+    var result = toPigLatin(words);
     // displayResult(result);
   });
 });
