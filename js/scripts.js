d4a3286bbbc10d270ec6df1b73daf40001a6489c
Abel Trotter and Eliran Maimon <abel@>
2018-06-06 11:56:16 -0700

    Add function to parse words in array and parse letters

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,35 @@
+const VOWELS = ["a", "e", "i", "o", "u", "y"];
+
 //Business Logic
 function toPigLatin(input) {
-  if (typeOf(input) !== "string") {
-    return input;
-  } else {
-    var words = parsePhrase(input);
-    
-  }
+  console.log(input);
+  var words = parsePhrase(input);
+  console.log(words);
+  parseWords(words);
 }
 
 function parsePhrase(userInput) {
-  return userInput.split(" ");
+  var words = userInput.split(" ");
+  return words;
+}
+
+function parseWords(wordsArray) {
+  for(var i = 0; i < wordsArray.length; i++) {
+    var lettersArray = wordsArray[i].split("");
+
+    if (VOWELS.includes(lettersArray[0])) {
+      var newWord = addWayToEnd(wordsArray[i]);
+    }
+
+    lettersArray.forEach(function(letter) {
+      //do something to each letter
+    });
+  }
+}
+
+function addWayToEnd(word) {
+  //add way to the end of string
+  console.log("add way to end of the word");
 }
 
 function displayResult(input) {
@@ -18,12 +38,10 @@ function displayResult(input) {
 
 // UI Logic
 $(document).ready(function() {
-  console.log("hello");
   $("#pig-latin").submit(function(event) {
     event.preventDefault();
     var userInput = $("#input").val();
     var result = toPigLatin(userInput);
-    displayResult(result);
+    // displayResult(result);
   });
-
 });
